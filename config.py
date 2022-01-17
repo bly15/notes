0b3f49e9f9654ccf89fc48ae79873ca5165ee7c4
Brian Ly <17816800+bly15@users.noreply.github.com>
2022-01-17 01:53:26 -0800

    Create initial models and migrate database

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ db_port = os.environ.get("DB_PORT", default="5432")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = (
-    f"postgres://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
