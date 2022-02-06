f5d1083b1ede06fc8ef030ac8d172692ae74024d
Brian Ly <17816800+bly15@users.noreply.github.com>
2022-02-05 22:12:47 -0800

    Change DB host

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-db_host = os.environ.get("DB_HOST", default="localhost")
+db_host = os.environ.get("DB_HOST", default="35.87.175.36")
 db_name = os.environ.get("DB_NAME", default="notes")
 db_user = os.environ.get("DB_USERNAME", default="notes")
 db_password = os.environ.get("DB_PASSWORD", default="")
